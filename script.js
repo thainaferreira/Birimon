d2b3a0e514910576b6008c797b8c1ba189a60ee3
Fernando Schneider <fernandomuniz2000@hotmail.com>
2021-04-05 13:37:13 -0300

    Creating structures for html, css and js

---
+++ script.js
@@ -0,0 +1,11 @@
+//CREATING PLAYERS
+    //class player_'red'
+    //class player_'black'
+
+//CREATING PLAYERS
+
+//CREATING TABLE
+const tab = document.getElementById('tab');
+    //id = section_'num' (7 total)
+    //class = 'col' (7 total)
+//CREATING TABLE
