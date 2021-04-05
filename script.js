5709c7e930e0daac55f2631142469cdded8203e1
Juliana Silva <juliana.os26@hotmail.com>
2021-04-05 15:00:09 -0400

    Creating function createTab

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //CREATING PLAYERS
     //class player_'red'
     //class player_'black'
-
 //CREATING PLAYERS
 
 //CREATING TABLE
 const tab = document.getElementById('tab');
-    //id = section_'num' (7 total)
-    //class = 'col' (7 total)
+const createTab = () => {
+    for (let i=0; i<7; i++) {
+        let newSection = document.createElement('section')
+        newSection.id = 'section_' + i
+        newSection.classList.add('col')
+        tab.appendChild(newSection)
+    }
+}
+createTab()
 //CREATING TABLE
